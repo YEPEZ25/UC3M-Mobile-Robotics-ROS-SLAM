dce0b6ecf299047286a4e7844d56d2e7972f0c98
YEPEZ25 <100505872@alumnos.uc3m.es>
2024-03-04 22:20:18 +0100

    Folder robots_moviles update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,21 @@
+# CMake generated Testfile for 
+# Source directory: /home/johnny/catkin_ws/src
+# Build directory: /home/johnny/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_ros_pkgs/gazebo_dev")
+subdirs("gazebo_ros_pkgs/gazebo_ros_pkgs")
+subdirs("turtlebot3/turtlebot3")
+subdirs("turtlebot3/turtlebot3_navigation")
+subdirs("gazebo_ros_pkgs/gazebo_msgs")
+subdirs("gazebo_ros_pkgs/gazebo_ros")
+subdirs("robots_moviles")
+subdirs("turtlebot3/turtlebot3_bringup")
+subdirs("turtlebot3/turtlebot3_example")
+subdirs("turtlebot3/turtlebot3_slam")
+subdirs("turtlebot3/turtlebot3_teleop")
+subdirs("gazebo_ros_pkgs/gazebo_plugins")
+subdirs("gazebo_ros_pkgs/gazebo_ros_control")
+subdirs("turtlebot3/turtlebot3_description")
