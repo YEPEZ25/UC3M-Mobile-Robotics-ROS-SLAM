3316916066b46301f29d52455359d830ef0e1592
YEPEZ25 <146430918+YEPEZ25@users.noreply.github.com>
2024-03-04 22:55:05 +0100

    Delete src/robots_moviles directory

--- src/robots_moviles/src/build/robots_moviles/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/johnny/catkin_ws/src/robots_moviles
-# Build directory: /home/johnny/catkin_ws/build/robots_moviles
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
